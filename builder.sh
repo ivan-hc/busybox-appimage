0eb5a12ee0b25c04317523fc7eb930067ccbe17c
iVAN <88724353+ivan-hc@users.noreply.github.com>
2026-04-09 18:12:48 +0200

    Add builder.sh

---
+++ builder.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+APP=busybox.static
+
+# TEMPORARY DIRECTORY
+mkdir -p tmp
+cd ./tmp || exit 1
+
+# DOWNLOAD APPIMAGETOOL
+if ! test -f ./appimagetool; then
+	wget -q https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage -O appimagetool || exit 1
+	chmod a+x ./appimagetool
+	export PATH="$PWD":"${PATH}"
+fi
+
+# CREATE BUSYBOX APPIMAGES
+
+_create_appimage(){
+	PKGNAME=$(curl -Ls https://dl-cdn.alpinelinux.org/alpine/latest-stable/main/"$ARCH_REF"/ | tr '">< ' '\n' | grep busybox-static | head -1)
+	VERSION=$(echo $PKGNAME | grep -Eo "[0-9].*[0-9]")
+	DL="https://dl-cdn.alpinelinux.org/alpine/latest-stable/main/x86_64/$PKGNAME"
+	if wget --version | head -1 | grep -q ' 1.'; then
+		wget -q --no-verbose --show-progress --progress=bar "$DL" -O "${a}${PKGNAME}" || exit 1
+	else
+		wget "$DL" || exit 1
+	fi
+	export arch="$a"
+	curl -Ls "https://raw.githubusercontent.com/ivan-hc/portable2appimage/refs/heads/main/portable2appimage" | sh -s -- ./*.apk busybox.static "$VERSION" "ivan-hc|Busybox-appimage|latest" || exit 1
+}
+
+ARCHITECTURES="i686 x86_64 aarch64"
+for a in $ARCHITECTURES; do
+	if [ "$a" = i686 ]; then
+		ARCH_REF="x86"
+	else
+		ARCH_REF="$a"
+	fi
+	mkdir -p "$a"
+	#cp ./appimagetool ./"$a"/appimagetool 2>/dev/null
+	cd "$a" || exit 1
+	_create_appimage
+	cd .. || exit 1
+	mv ./"$arch"/*.AppImage* ./
+done
+
+cd ..
+mv ./tmp/*.AppImage* ./
